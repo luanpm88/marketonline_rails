58e85d145668c8ef50ac69be2876fbc65e853ad0
Luan Pham <luanpm88@gmail.com>
2015-11-12 19:37:10 -0800

    update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   before_filter :app_variable
   
   rescue_from CanCan::AccessDenied do |exception|
-    redirect_to "/logging.php?return_page=#{Rack::Utils.escape(root_path(:only_path => false))}"
+    redirect_to "/logging.php?return_page=#{Rack::Utils.escape(request.fullpath)}"
   end
   
   def app_variable
