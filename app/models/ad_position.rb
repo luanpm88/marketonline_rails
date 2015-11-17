08bd30a24f8cd73e49472d3b6cd5a390511e1940
Luan Pham <luanpm88@gmail.com>
2015-11-17 00:42:05 -0800

    active ad

--- app/models/ad_position.rb
+++ app/models/ad_position.rb
@@ -7,7 +7,7 @@ class AdPosition < ActiveRecord::Base
   end
   
   def active_ads
-    ads.where(status: 1).limit(number_of_ad)
+    ads.where(active: 1).limit(number_of_ad)
   end
   
   def width=(new)
