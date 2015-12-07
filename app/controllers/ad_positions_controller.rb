7c97a140304ec356fdbc18c240e5bbe3469ec1ac
Luan Pham <luanpm88@gmail.com>
2015-12-07 01:01:59 -0800

    update

--- app/controllers/ad_positions_controller.rb
+++ app/controllers/ad_positions_controller.rb
@@ -70,11 +70,13 @@ class AdPositionsController < ApplicationController
   end
 
   def iframe_home_top_banner
-    @pos_1 = AdPosition.get("home_top_banner_1")
-    @pos_2 = AdPosition.get("home_top_banner_2")
-    @pos_3 = AdPosition.get("home_top_banner_3")
+    
+    render text: request.remote_ip
+    #@pos_1 = AdPosition.get("home_top_banner_1")
+    #@pos_2 = AdPosition.get("home_top_banner_2")
+    #@pos_3 = AdPosition.get("home_top_banner_3")
 
-    render layout: "ad_frame"
+    #render layout: "ad_frame"
   end
   
   def preview_box
