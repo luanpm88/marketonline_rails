3259088afe371942a07c09f853a4ed9c57746301
Luan Pham <luanpm88@gmail.com>
2015-12-15 20:27:56 -0800

    area select

--- app/models/pb_area.rb
+++ app/models/pb_area.rb
@@ -2,7 +2,7 @@ class PbArea < ActiveRecord::Base
   belongs_to :parent, class_name: "PbArea", foreign_key: "parent_id"
   
   def self.general_search(params, user)
-    result = self.all
+    result = self.where.not(id: 1)
     result = result.where("LOWER(pb_areas.name) LIKE ?", "%#{params[:q].strip.downcase}%")
    
     result = result.limit(50).map {|model| {:id => model.id, :text => model.full_name_inverse} }
