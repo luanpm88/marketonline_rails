e2655b172c60d12e773f9b737f0b58eed60c6a1a
Luan Pham <luanpm88@gmail.com>
2015-12-21 20:30:02 -0800

    update

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -25,12 +25,12 @@ class Deal < ActiveRecord::Base
     @records.each do |item|
       row = [
               "<div class=\"\"><img src=\"#{item.pb_product.default_image}\" width=\"100\" /></div>",
-              "<div class=\"\"><a target=\"_blank\" href=\"#{item.pb_product.url}\">#{item.pb_product.name}</a></div>",
-              "<div class=\"\">#{ApplicationController.helpers.format_price(item.price)}</div>",
+              "<div class=\"\"><a target=\"_blank\" href=\"#{item.pb_product.url}\">#{item.pb_product.name}</a><br/>#{item.description}</div>",
+              "<div class=\"\">#{item.display_price}</div>",
               "<div class=\"\">#{item.pb_product.price_unit}</div>",
               "<div class=\"\">#{ApplicationController.helpers.format_price(item.remain_items_count.to_s)}</div>",
               "<div class=\"\">#{ApplicationController.helpers.format_price(item.sold_items_count.to_s)}/#{ApplicationController.helpers.format_price(item.quantity.to_s)}</div>",
-              "<div class=\"text-nowrap\">Từ: #{item.start_at.strftime("%d-%m-%Y")}<br />Đến: #{item.end_at.strftime("%d-%m-%Y")}</div>",
+              "<div class=\"text-nowrap\">#{item.display_time}</div>",
               "<div class=\"\"></div>",
               "<div class=\"text-right\"><ul class=\"icons-list\">"+
                   "<li class=\"dropup\">"+
@@ -53,6 +53,40 @@ class Deal < ActiveRecord::Base
     result["data"] = data
     
     return {result: result}
+  end
+  
+  def share_amout
+    price*(1.0-share_price/100.0)
+  end
+  
+  def agent_amount
+    price*(1.0-agent_price/100.0)
+  end
+  
+  def display_price
+    str = ["<div class=\"text-nowrap deal_label_col\">"]
+    str << "<label>Giá gốc: </label>"
+    str << "<div><span class=\"text-bold\">"+ApplicationController.helpers.format_price(price)+"</span></div>"
+    str << "<label>Giá cho cộng tác viên: </label>"
+    str << "<div><span class=\"text-bold\">"+ApplicationController.helpers.format_price(agent_amount)+"</span> (giảm #{agent_price}%)</div>"
+    str << "<label>Giá người được giới thiệu: </label>"
+    str << "<div><span class=\"text-bold\">"+ApplicationController.helpers.format_price(share_amout)+"</span> (giảm #{share_price}%)</div>"
+    str << "</div>"
+    
+    return str.join("")
+  end
+  
+  def display_time
+    str = ["<div class=\"text-nowrap deal_label_col\">"]
+    str << "<label>Bắt đầu ngày: </label>"
+    str << "<div><span class=\"text-bold\">"+start_at.strftime("%d-%m-%Y")+"</span></div>"
+    str << "<label>Kết thúc ngày: </label>"
+    str << "<div><span class=\"text-bold\">"+end_at.strftime("%d-%m-%Y")+"</span></div>"
+    str << "<label>Còn lại: </label>"
+    str << "<div><span class=\"text-bold\">"+(end_at.to_date - Date.today).to_i.to_s+"</span> ngày</div>"
+    str << "</div>"
+    
+    return str.join("")
   end
   
   def sold_items_count
