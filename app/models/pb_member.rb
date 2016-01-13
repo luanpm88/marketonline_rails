ba076f9cf2770a5b2d6b93e4eb2a9aac247c15ef
Luan Pham <luanpm88@gmail.com>
2016-01-13 00:48:18 -0800

    update

--- app/models/pb_member.rb
+++ app/models/pb_member.rb
@@ -27,7 +27,8 @@ class PbMember < ActiveRecord::Base
   has_many :agent_payments
   
   def display_name
-    return username if !pb_memberfield.present? or !pb_memberfield.first_name.present? and !pb_memberfield.last_name.present?
+    return username if !pb_memberfield.present?
+    return username if !pb_memberfield.first_name.present? and !pb_memberfield.last_name.present?
     pb_memberfield.first_name+" "+pb_memberfield.last_name
   end
   
